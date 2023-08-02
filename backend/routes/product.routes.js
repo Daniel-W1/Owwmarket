7e823fc0795583feaa5169e887ae136dd9a98205
daniel-w1 <wossendaniel7@gmail.com>
2023-08-02 09:33:21 +0300

    proudct image upload finished

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import authController from '../controllers/auth.controller.js';
 import productController from '../controllers/product.controller.js';
+import shopController from '../controllers/shop.controller.js';
 
 const router = express.Router()
 
-router.route('/products').get(authController.requireSignin, authController.isAdmin, productController.list).post(authController.requireSignin, authController.hasAuthorization, productController.create);
-router.route('/products/:productId').get( productController.read).put(authController.requireSignin, authController.hasAuthorization, productController.update).delete(authController.requireSignin, authController.hasAuthorization, productController.remove);
+router.get('/products', productController.list);
+router.route('/from/:shopId/products').get(authController.requireSignin, productController.list).post(authController.requireSignin, authController.hasAuthorization, productController.create);
+router.route('/products/:productId/images').get(productController.photo, productController.defaultPhoto);
+router.route('/from/:shopId/products/:productId').get( productController.read).put(authController.requireSignin, authController.hasAuthorization, productController.update).delete(authController.requireSignin, authController.hasAuthorization, productController.remove);
 router.param('productId', productController.productByID);
+router.param('shopId', shopController.shopbyId);
 
 export default router;
