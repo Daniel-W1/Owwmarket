53592ee7a369964d298e485911a629a39e800a90
l1stman <l1stman>
2023-09-03 16:11:21 +0100

    This is Create Shop code Not working!

--- frontend/oww-market/src/pages/shops.jsx
+++ frontend/oww-market/src/pages/shops.jsx
@@ -1,53 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { GetShopForUser, GetUserById } from '../hooks/helpers';
 import LoadingScreen from '../components/loading';
 import ShopCard from '../components/ShopCard';
 import { useParams } from 'react-router-dom';
+import CreateShop from '../components/CreateShop';
 
-
-const Shops = () => {
-
+const Shops = (props) => {
   const params = useParams();
-  const [loading, setloading] = useState(true)
-  const [shops, setshops] = useState([])
-  const [user, setuser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shops, setShops] = useState([]);
+  const [user, setUser] = useState(null);
 
-  const the_userId = params.userId ? params.userId: JSON.parse(localStorage.getItem("user"))._id;
+  const the_userId = params.userId ? params.userId : JSON.parse(localStorage.getItem("user"))._id;
+
   useEffect(() => {
-    (
-      async () => {
-        const response = await GetShopForUser(the_userId).then((res) => {
-          return res;
-        });
+    async function fetchData() {
+      try {
+        const user = await GetUserById(the_userId);
+        setUser(user);
 
-        const user = GetUserById(the_userId);
+        if (props.type === "create") {
+          // Handle create mode if needed
+        } else {
+          const response = await GetShopForUser(the_userId);
+          setShops(response);
+        }
 
-        setuser(user)
-        setshops(response)
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-      ) ();
-    }, [])
-    
-    useEffect(() => {
-      if (shops && user) {
-        setloading(false)
-      }
-    }, [shops, user])
+    }
+
+    fetchData();
+  }, [the_userId, props.type]);
 
   return (
     <div>
-      {
-        loading ? <LoadingScreen text={'loading..'} /> : (
-          shops.map((shop) => {
-            return (
-              <ShopCard shop = {shop} />
-            )
-          })
-
-        )
-      }
+      {loading ? (
+        <LoadingScreen text={'loading..'} />
+      ) : props.type !== "create" ? (
+        shops.map((shop) => {
+          return <ShopCard shop={shop} key={shop.id} />;
+        })
+      ) : (
+        <>
+          <CreateShop userId={the_userId} />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Shops
+export default Shops;
