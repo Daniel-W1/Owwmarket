b97da8ac4996525860d4f2c514268f5d60d5dea4
daniel-w1 <wossendaniel7@gmail.com>
2023-09-07 23:59:46 +0300

    infinite scrolling added

--- frontend/oww-market/src/components/PostCard.jsx
+++ frontend/oww-market/src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import tw from 'twin.macro'
 import { motion } from 'framer-motion'
 import profileImage from '../assets/images/twitter-icon.png'
@@ -6,6 +6,9 @@ import { ReactComponent as StarIcon } from "../assets/images/star-icon.svg";
 import { Carousel } from 'react-responsive-carousel';
 import styled from 'styled-components';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { imagefrombuffer } from "imagefrombuffer"; //first import 
+import { GetProfileForUser, GetShopById, GetShopForUser } from '../hooks/helpers';
+import LoadingScreen from './loading';
 
 const images = [
     profileImage,
@@ -18,32 +21,69 @@ const SvgContainer = styled.div`
     }
 `
 
-const PostCard = () => {
-    const [isHovering, setIsHovering] = useState(true);
+const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" }
+    return new Date(dateString).toLocaleDateString(undefined, options)
+}
 
-    return (
+const PostCard = ({ post }) => {
+    const [loading, setloading] = useState(true);
+    const [isHovering, setIsHovering] = useState(true);
+    const [profile, setprofile] = useState(null);
+    const [shop, setshop] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const response = await GetProfileForUser(post.owner)
+            const shop_response = await GetShopById(post.shopId)
+
+            const real_profile = response.profile;
+
+            setshop(shop_response)
+            setprofile(real_profile)
+        }
+
+        )();
+    }, [])
+
+    useEffect(() => {
+        if (profile && shop) {
+            setloading(false)
+        }
+    }, [profile, shop])
+
+
+    return ( loading ? <LoadingScreen text={'loading..'} /> :
         <div className='flex-col w-full justify-center items-center mx-auto border-2 border-gray-200 rounded-xl overflow-hidden'>
             <div className='flex flex-wrap justify-start items-center px-2 py-2 bg-gray-200'>
                 <div className='w-12 h-12  lg:w-16 lg:h-16 mx-3 rounded-full'>
-                    <img src={profileImage} alt="profile image" className='w-full h-full rounded-full' />
+                    <img src={
+                        imagefrombuffer(
+                            { data: profile?.image.data.data }
+                        )
+                    } alt="profile image" className='w-full h-full rounded-full' />
                 </div>
                 <div className='flex-col items-start'>
                     <div className='text-xl'>
-                        John Doe
+                        {profile.name}
                     </div>
                     <div className='text-sm text-gray-500'>
-                        2 hours ago
+                        {formatDate(post.createdAt)}
                     </div>
                 </div>
             </div>
 
             <div className='w-full'>
-                <Carousel emulateTouch = {true} className='w-full'>
+                <Carousel emulateTouch={true} className='w-full'>
                     {
-                        images.map((image, index) => {
+                        post.productImages.map((image, index) => {
                             return (
                                 <div key={index} className='h-72'>
-                                    <img src={image} className='h-full' alt="post image" />
+                                    <img src={
+                                        imagefrombuffer(
+                                            { data: image.data.data }
+                                        )
+                                    } className='h-full' alt="post image" />
                                 </div>
                             )
                         })
@@ -53,23 +93,21 @@ const PostCard = () => {
 
             <div className='flex-col items-start px-4'>
                 <div className='text-xl'>
-                    Product Name
+                    {post.productname}
                 </div>
                 <div className='text-sm text-gray-500 flex'>
-                    <span>Shop Name</span>
+                    <span>{shop.name}</span>
                     <span className='mx-2'>|</span>
                     <span className='flex items-center'>
                         <SvgContainer>
                             <StarIcon className='w-4 h-4' />
                         </SvgContainer>
-                        <span>4.8 (200)</span>
+                        <span>{Math.round(Math.random()*50)/10} {Math.round(Math.random()*200)}</span>
                     </span>
                 </div>
                 <hr className='w-full bg-gray-600 my-2' />
                 <div className='my-2'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, doloremque dolores 
-                    cupiditate corrupti molestiae earum voluptatibus. 
-                    Aperiam autem aspernatur omnis quibusdam quos suscipit deserunt, ad aliquam consequuntur eum odio hic!
+                    {post.productdescription}
                 </div>
 
                 <button className='my-2 border-none bg-primary-200 rounded-lg text-white px-2 py-1 lg:px-3 lg:py-2'>
--- frontend/oww-market/src/pages/feed.jsx
+++ frontend/oww-market/src/pages/feed.jsx
@@ -1,55 +1,88 @@
-import React, { useEffect, useState } from 'react'
-import Sidebar from '../components/Sidebar'
-import PostCard from '../components/PostCard'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
 import FeedSidebar from '../components/FeedSidebar'
 import RightSidecard from '../components/RightSidecard'
 import useScreenSize from '../hooks/useScreenSize'
+import { GetFeedForUser } from '../hooks/helpers'
+import LoadingScreen from '../components/loading'
+import PostCard from '../components/PostCard'
+import { imagefrombuffer } from "imagefrombuffer"; //first import 
+
 
 const Feed = () => {
-
     const [loading, setloading] = useState(true);
+    const [posts, setposts] = useState([]);
+    const [pageNum, setpageNum] = useState(1);
+    const [hasMore, sethasMore] = useState(false);
+
+    const observer = useRef();
+    const lastPostElementRef = useCallback(node => {
+        if (loading) return;
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasMore) {
+                setpageNum(prevPageNum => prevPageNum + 1)
+            }
+        })
+        if (node) observer.current.observe(node)
+    }, [loading, hasMore])
 
     useEffect(() => {
         (
             async () => {
-                
+                const response = await GetFeedForUser(pageNum);
+                const real_posts = response.posts;
+
+                sethasMore(posts?.length + 3 < response.total)
+                setposts([...posts, ...real_posts]);
             }
         )()
     }, [])
 
-    const posts = [
-        <PostCard/>,
-        <PostCard/>,
-        <PostCard/>,
-        <PostCard/>,
-        <PostCard/>,
-        <PostCard/>,
-        <PostCard/>,
-        <PostCard/>,
-        <PostCard/>,
-        <PostCard/>,
-    ]
+    useEffect(() => {
+        (
+            async () => {
+                const response = await GetFeedForUser(pageNum);
+                const real_posts = response.posts;
+
+                console.log(response);
+
+                sethasMore(posts?.length + 3 < response.total)
+                setposts([...posts, ...real_posts]);
+            }
+        )()
+    }, [pageNum])
+
+    useEffect(() => {
+        if (posts.length > 0) {
+            setloading(false)
+        }
+    }, [posts])
 
     const screenSize = useScreenSize();
 
-  return (
-    <div className={`flex w-full justify-center px-10 md:justify-around lg:justify-around py-10`}>
-        <FeedSidebar/>
-        <div className='flex-col w-3/4 md:w-1/2 lg:w-1/2'>
-            {
-                posts.map((post, index) => {
-                    return (
-                        <div key={index} className='mb-4'>
-                            {post}
-                        </div>
+    return (
+        <div className={`flex w-full justify-center px-10 md:justify-around lg:justify-around py-10`}>
+            <FeedSidebar />
+            {loading ? <LoadingScreen text={'loading..'} /> : <div className='flex-col w-3/4 md:w-1/2 lg:w-1/2'>
+                {
+                    posts.map((post, index) => {
+                        return (index === posts.length - 1 ?
+                            <div ref={lastPostElementRef} key={index}>
+                                <PostCard post={post} />
+                            </div>
+                            :
+                            <div key={index}>
+                                <PostCard post={post} />
+                            </div>
+                        )
+
+                    }
                     )
                 }
-                )
-            }
+            </div>}
+            <RightSidecard />
         </div>
-        <RightSidecard />
-    </div>
-  )
+    )
 }
 
 export default Feed
