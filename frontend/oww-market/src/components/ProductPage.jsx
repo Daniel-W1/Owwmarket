372f39ba36f941c30c621dadaf5c41b8932b0945
l1stman <l1stman>
2023-09-10 17:30:12 +0100

    Fixing some Bugs + submit a bid (only frontend)

--- frontend/oww-market/src/components/ProductPage.jsx
+++ frontend/oww-market/src/components/ProductPage.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { MdHotelClass, MdShoppingCart } from "react-icons/md";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { MdHotelClass, MdShoppingCart, MdSend, MdClose } from "react-icons/md";
 import LoadingScreen from "./loading";
 import {
   GetProductsForShop,
@@ -23,18 +23,17 @@ const ProductPage = () => {
 
   const [loading, setLoading] = useState(true);
   const [product, setProduct] = useState("");
-  const [name, setName] = useState("");
-  const [shop, setShop] = useState(null);
-  const [allproducts, setAllproducts] = useState(null);
-  const [profile, setProfile] = useState(null);
-  const [bids, setBids] = useState([
-    { user: 'User 1', bid: 100, biddingAt: new Date("2023-09-09T10:10:00Z") },
-    { user: 'User 2', bid: 120, biddingAt: new Date("2023-09-09T10:20:00Z")},
-    { user: 'User 3', bid: 150, biddingAt: new Date("2023-09-09T10:30:00Z")},
-  ]);
+  const [shop, setShop] = useState("");
+  const [allproducts, setAllproducts] = useState("");
+  const [profile, setProfile] = useState("");
+  const [bids, setBids] = useState("");
   
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [bidprice, setBidprice] = useState(0)
+  
+  const [modalerror, setModalerror] = useState("");
+  const [success, setSuccess] = useState(false);
+
   const handleIncrease = () => {
     // Get the available stock from the product object
     const availableStock = product.itemsLeft;
@@ -51,25 +50,43 @@ const ProductPage = () => {
   };
 
   const openModal = () => {
+    setBidprice(bids[0].bid + 1) // add one dollar for make it start with example(150$ top price we should put 151$ for bidding) 
     setIsModalOpen(true);
   };
-
+  
   const closeModal = () => {
     setIsModalOpen(false);
   };
-
+  
   const handleBidChange = (e) => {
-    setBidprice(e.target.value);
-  };
+    const newBid = parseFloat(e.target.value);
 
+  // Check if the newBid is greater than or equal to the initial bid (bids[0].bid)
+  if (!isNaN(newBid) && newBid > (bids.length === 0 ? product.startedprice : bids[0].bid)) {
+    setModalerror("")
+  } else {
+    setModalerror(`you can't bid with a price less than $${bids.length === 0 ? product.startedprice : bids[0].bid}`)
+    // Handle the case where the newBid is invalid (less than initial bid)
+    // You can show an error message or take any other appropriate action.
+  }
+  setBidprice(newBid);
+
+   
+  };
+  
   const handleSubmitBid = () => {
     // Handle the submitted number here, e.g., you can log it to the console
-    console.log("Submitted number:", number);
-
+    console.log("Submitted number:", bidprice);
+    
     // Close the modal
     closeModal();
+    setModalerror("");
+    setSuccess(true);
+    setTimeout(() => {
+      setSuccess(false)
+    }, 3000);
   };
-
+  
   function formatDate(date) {
     const options = { 
       day: 'numeric',
@@ -84,26 +101,29 @@ const ProductPage = () => {
   useEffect(() => {
     (async () => {
       try {
-        const shop = await GetShopById(params.shopId);
-          setShop(shop);
-        const allproducts = await GetProductsForShop(params.shopId, shop.owner);
-          setAllproducts(allproducts.products);
-        const product = await GetProductById(params.shopId, params.productId);      
-          // setBids(product.bids.sort((a, b) => new Date(b.biddingAt) - new Date(a.biddingAt)));
-          setBids(product.bids.slice().sort((a, b) => b.bid - a.bid))
-        const profile = await GetProfileForUser(shop.owner);
-          setProfile(profile);
-        const { name } = profile.profile;
-        setName(name);
-        setProduct(product);
+        const p = await GetProductById(params.shopId, params.productId);      
+        setProduct(p);
+        if(p.auction) {
+          setBids(p.bids.slice().sort((a, b) => b.bid - a.bid))
+        }
+        const s = await GetShopById(params.shopId);
+        setShop(s);
+        const allproducts = await GetProductsForShop(params.shopId, s.owner);
+        setAllproducts(allproducts.products);
+        // setBids(product.bids.sort((a, b) => new Date(b.biddingAt) - new Date(a.biddingAt)));
+        
+        const pr = await GetProfileForUser(s.owner);
+        setProfile(pr);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
         setLoading(false);
       }
     })();
-  }, [params.shopId, params.productId]);
-
+  }, []);
+  
+ 
+  
   if (loading) {
     return <LoadingScreen text={"Loading..."} />;
   }
@@ -173,39 +193,66 @@ const ProductPage = () => {
           </div>
           {product.auction ? (
             <>
-            <button className="bg-black text-white flex items-center justify-center font-semibold py-3 px-16 rounded-xl h-full">
+            <button onClick={openModal} className="bg-black text-white flex items-center justify-center font-semibold py-3 px-16 rounded-xl h-full">
               <MdHotelClass />
-              <span className="ml-2" onClick={openModal}>Bid Now</span>
+              <span className="ml-2">Bid Now</span>
             </button>
 
+            {/* Modal */}
+            <div
+      className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${
+        isModalOpen || success ? 'block' : 'hidden'
+      } bg-black bg-opacity-50`}
+    >
             {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="bg-white w-1/3 p-6 rounded-lg shadow-lg">
-            <h2 className="text-xl font-semibold mb-4">Place a Bid</h2>
+        <div className="bg-white w-1/3 p-6 rounded-lg shadow-lg">
+          <h2 className="text-xl font-semibold mb-4">Place a Bid</h2>
+          {modalerror && (
+            <p className="text-red-600 text-center text-sm mb-4">{modalerror}</p>
+          )}
+          <div className="flex flex-col">
             <input
               type="number"
-              className="w-full p-2 border border-gray-300 rounded-md mb-4"
+              className={`w-full p-2 border rounded-md mb-2 outline-none ${
+                modalerror ? 'border-red-600' : 'border-gray-300'
+              }`}
               placeholder="Enter bid amount"
               value={bidprice}
               onChange={handleBidChange}
             />
             <div className="flex justify-end">
               <button
-                className="bg-black text-white py-2 px-4 rounded-md"
+                className={`flex items-center justify-center bg-black text-white py-2 px-8 rounded-md ${
+                  modalerror ? 'cursor-not-allowed' : null
+                }`}
                 onClick={handleSubmitBid}
+                disabled={modalerror}
               >
-                Submit Bid
+                 <MdSend />
+              <span className="ml-2">Submit Bid</span>
               </button>
               <button
-                className="ml-2 text-gray-500 hover:text-gray-700"
+                className="ml-2 text-black-500 text-xl"
                 onClick={closeModal}
               >
-                Close
+                <MdClose/>
               </button>
             </div>
           </div>
         </div>
+      </div>
       )}
+      {success && (
+        <div className="success-animation">
+        <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none" /><path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /></svg>
+            <center>
+            <p className='text-white font-semibold mt-4 text-3xl'>Your bid has been placed successfully.</p>
+            </center>
+       </div>
+      )}
+    </div>
+     
             </>
           ) : (
             <button className="bg-black text-white flex items-center justify-center font-semibold py-3 px-16 rounded-xl h-full">
